85e8b059cee9ba7808337e75bdef9c0560a71396
ih24254 <ih24254@students.sacredheart.school.nz>
2025-07-16 23:58:39 +0000

    Age Commit

--- Website.py
+++ Website.py
@@ -1,3 +1,11 @@
 from bottle import route, run, template, view, static_file, request, redirect, error
 import requests
-from dataline import dataline
+from datetime import datetime
+@route('/')
+def index():
+    name = 'Isaiah'
+    response = requests.get(f"https://api.agify.io/?name={name}")
+    response = response.json()
+    age = response['age']
+    return template('index', name=name, age=age)
+run(host='0.0.0.0', port=4000, reloader=True, debug=True)
