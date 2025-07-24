d11fdb280fa50b8fdd24cc91bb8c3c185bd44d84
ih24254 <ih24254@students.sacredheart.school.nz>
2025-07-24 00:00:09 +0000

    good

--- Website.py
+++ Website.py
@@ -1,9 +1,11 @@
 from bottle import route, run, template, view, static_file, request, redirect, error
 import requests
 from datetime import datetime
-@route('/')
-def index():
-   now = datetime.now()
-   current_time = now.strftime("%H:%M:%S")
-   return template('index', time=current_time)
+
+@route('/<name>')
+def index(name):
+     response = requests.get(f"https://api.agify.io/?name={name}")
+     response = response.json()
+     age = response['age']
+     return template('index', name=name, age=age)
 run(host='0.0.0.0', port=4000, reloader=True, debug=True)
