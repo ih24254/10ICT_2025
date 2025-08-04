8c3d27b95815f258c26341c668956c63d194fcfc
ih24254 <ih24254@students.sacredheart.school.nz>
2025-08-04 00:10:27 +0000

    9th change

--- Website.py
+++ Website.py
@@ -2,12 +2,15 @@ from bottle import route, run, template, view, static_file, request, redirect, e
 import requests
 from datetime import datetime
 
+@route('/static/<filepath:path>')
+def server_static(filepath):
+  return static_file(filepath, root='./static')
 
 @route('/')
 @route('/home')
 @view('homepage')
 def index():
-     pass
+  pass
 
 #main routine
 run(host='0.0.0.0', port=4000, reloader=True, debug=True)
